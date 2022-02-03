466ad5538484fcaee7b4809fa6370d9a1a216ec8
Esther Aguado <e.aguado.glez@gmail.com>
2022-02-03 15:27:17 +0100

    add sensors and cave world

--- README.md
+++ README.md
@@ -26,4 +26,28 @@ Launch the simulation:
 ```
 ros2 launch ign_simulation x1_empty.launch.py
 ```
+or:
+```
+ros2 launch ign_simulation x1_empty.launch.py  ign_args:="cave_world.sdf" 
+```
+
+
+List ROS and ignition topics, not all ignition topics are mapped, only used ones:
+```
+ros2 topic list
+ign topic -l
+```
+Echo ROS and ignition topics:
+```
+ros2 topic echo /model/x1_ugv/odometry  
+
+ign topic -e -t /model/x1_ugv/odometry
+```
+
+Send ROS topics or ignition topics:
+```
+ros2 topic pub --once /cmd_vel geometry_msgs/msg/Twist '{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 1.8}}'
+
+ign topic -t "/cmd_vel" -m ignition.msgs.Twist -p "linear: {x: 0}, angular: {z: 0.0}"
+```
 
--- launch/x1_empty.launch.py
+++ launch/x1_empty.launch.py
@@ -19,60 +19,67 @@ def generate_launch_description():
     ign_gazebo = IncludeLaunchDescription(
       PythonLaunchDescriptionSource(
       os.path.join(pkg_ros_ign_gazebo, 'launch', 'ign_gazebo.launch.py')),
-      launch_arguments={'ign_args': os.path.join(pkg, 'worlds', 'empty.sdf')}.items(),
+      #launch_arguments={'ign_args': os.path.join(pkg, 'worlds', 'empty.sdf')}.items(),
      )
-  
-  #  # Parse robot description from xacro
-  #   robot_description_file =  os.path.join(pkg, 'models', 'turtlebot3_description', 'urdf', 'turtlebot3_burger.urdf')
-  #   robot_description_config = xacro.process_file(
-  #       robot_description_file
-  #   )
-  #   robot_description = {"robot_description": robot_description_config.toxml()}
-
-    # # Robot state publisher
-    # robot_state_publisher = Node(
-    #   package='robot_state_publisher',
-    #   executable='robot_state_publisher',
-    #   name='robot_state_publisher',
-    #   output='both',
-    #   parameters=[robot_description],)
-
 
     spawn = Node(package='ros_ign_gazebo', executable='create',
                 arguments=[
                     '-name', 'x1_ugv',
-                    #'-topic', 'robot_description',
-                    '-file',  os.path.join(pkg, 'models', 'X1',
-                                          'model.sdf')
+                    '-file',  os.path.join(pkg, 'models', 'x1', 'model.sdf')
                     ],
                 output='screen',
                 )
     
-    # bridge = Node(
-    #   package='ros_ign_bridge',
-    #   executable='parameter_bridge',
-    #   arguments=[
-    #             # Joint states (IGN -> ROS2)
-    #            '/world/empty/model/turtlebot3_burger/joint_state@sensor_msgs/msg/JointState[ignition.msgs.Model',
-    #            '/world/empty/model/turtlebot3_burger/link/base_footprint/sensor/imu/imu@sensor_msgs/msg/Imu[ignition.msgs.IMU',
-    #             ],
-    #   remappings=[
-    #         ('/world/empty/model/turtlebot3_burger/joint_state', 'joint_states'),
-    #         ('/world/empty/model/turtlebot3_burger/link/base_footprint/sensor/imu/imu', 'imu'),
-    #     ],
-    #     output='screen')
+    bridge = Node(
+      package='ros_ign_bridge',
+      executable='parameter_bridge',
+      arguments=[
+      # Mappings (IGN -> ROS2)
+      # The ROS message type is followed by an @, [, or ] symbol where:
 
-    # rviz = Node(
-    #   package='rviz2',
-    #   executable='rviz2',
-    #   arguments=['-d', os.path.join(pkg,'models', 'turtlebot3_description', 'rviz', 'model.rviz')]
-    # )
-            
+      # @ is a bidirectional bridge.
+      # [ is a bridge from Ignition to ROS.
+      # ] is a bridge from ROS to Ignition.
+      # https://github.com/ignitionrobotics/ros_ign/tree/ros2/ros_ign_bridge
+
+      '/imu@sensor_msgs/msg/Imu[ignition.msgs.IMU',
+      '/lidar@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan',
+
+      '/model/x1_ugv/odometry@nav_msgs/msg/Odometry[ignition.msgs.Odometry',
+      '/model/x1_ugv/pose@geometry_msgs/msg/Pose[ignition.msgs.Pose',
+      '/model/x1_ugv/tf@tf2_msgs/msg/TFMessage[ignition.msgs.Pose_V',
+
+      '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
+      '/cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist',          
+
+      '/camera_front/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+      '/camera_front/depth_image@sensor_msgs/msg/Image[ignition.msgs.Image',
+      '/camera_front/image@sensor_msgs/msg/Image[ignition.msgs.Image',
+
+      '/camera_left/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+      '/camera_left/depth_image@sensor_msgs/msg/Image[ignition.msgs.Image',
+      '/camera_left/image@sensor_msgs/msg/Image[ignition.msgs.Image',
+
+      '/camera_rear/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+      '/camera_rear/depth_image@sensor_msgs/msg/Image[ignition.msgs.Image',
+      '/camera_rear/image@sensor_msgs/msg/Image[ignition.msgs.Image',
+
+      '/camera_right/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+      '/camera_right/depth_image@sensor_msgs/msg/Image[ignition.msgs.Image',
+      '/camera_right/image@sensor_msgs/msg/Image[ignition.msgs.Image',               
+             
+                ],
+      remappings=[
+            ('/imu', 'imu'),
+        ],
+        output='screen')
+   
     return LaunchDescription([
+      DeclareLaunchArgument(
+          'ign_args',
+          default_value=[os.path.join(pkg, 'worlds', 'empty.sdf')]),
         ign_gazebo,
-       # robot_state_publisher,
         spawn,
-        # bridge,
-       # rviz,
+        bridge,
     ])
 
